3c175a46cf4d84e569525e053db9bc99e96e6f75
Li XiaYu <liqixbd@hotmail.com>
2017-08-04 23:49:57 +0800

    v0.1.0.20170804 复制Resource的文件到对应的目录， 粗制滥造的简单UI

---
+++ MY_Cloud_notes_installation_package/CodeProgess.cs
@@ -0,0 +1,31 @@
+﻿using SevenZip;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MY_Cloud_notes_installation_package
+{
+    class CodeProgress : ICodeProgress
+    {
+        public delegate void ProgressDelegate(Int64 fileSize, Int64 processSize);
+
+        public ProgressDelegate m_ProgressDelegate = null;
+
+        public CodeProgress(ProgressDelegate del)
+        {
+            m_ProgressDelegate = del;
+        }
+
+        public void SetProgress(Int64 inSize, Int64 outSize)
+        {
+
+        }
+
+        public void SetProgressPercent(Int64 fileSize, Int64 processSize)
+        {
+            m_ProgressDelegate(fileSize, processSize);
+        }
+    }
+}
