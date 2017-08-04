8a16c04488d8091b83b08732d1e2267b7b90c9b8
王嘉宁 <evilrabbit520@gmail.com>
2017-08-04 20:38:51 +0800

    同步

---
+++ MY_Cloud_notes_installation_package/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MY_Cloud_notes_installation_package
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
